e8a40ffd01b0c8e52cfd04de2cd14fafdfe226e4
↺ʙᴧɢᴧsҡᴧʀᴧ↻ <93908715+ybgskr12@users.noreply.github.com>
2023-09-12 09:22:26 +0700

    Update logger.py

--- CilikMusic/utils/logger.py
+++ CilikMusic/utils/logger.py
@@ -19,7 +19,7 @@ async def play_logs(message, streamtype):
         else:
             chatusername = "Private Group"
         logger_text = f"""
-📮 **NandaPedia Play Logs**
+📮 **Bagaskara Music Logs**
 
 💬 **Chat:** {message.chat.title} [`{message.chat.id}`]
 👨🏻‍🚀 **User:** {message.from_user.mention}
